e9d4a365be16df201c497963f4adb97da24d9ace
Amiya Ranjan Sahu <amiyaranjansahu2003@gmail.com>
2025-06-21 09:24:37 +0530

    Use of Object , and some inbuild Math funtion in JavaScript

---
+++ Assignment4.js
@@ -0,0 +1,21 @@
+//Q1
+let roll = Math.floor(Math.random()*6)+1;
+console.log(`Dice ${roll}`);
+
+//2
+const car={
+    name : "BMW",
+    model : 3,
+    color : "Green"
+};
+console.log(`carName : ${car.name}`);
+
+//3
+const person={
+    name : "XYZ",
+    age : 21,
+    city : "Singhapur"
+};
+person.city = "NewYork";
+person.country = "United States";
+console.log(person);
